78e67cd04d25fd662d555af01553a7a7027b19f7
yashmali408 <yashmali408@gmail.com>
2023-12-06 18:30:38 +0530

    yash

---
+++ cylender.py
@@ -0,0 +1,8 @@
+r=input("pleace enter your radius")
+r=float(r)
+h=input("pleace enter tyour height")
+h=float(h)
+
+volumeofcynlider=(22/7)*r*r*h 
+
+print(f"the volum of cyender is {volumeofcynlider}")
---
+++ yash.py
@@ -0,0 +1,6 @@
+name= input("Please enter your name")
+
+age=input('enter you age here')
+
+
+print(f"hello{name} you age is {age}")
